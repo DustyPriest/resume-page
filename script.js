b9a7cefb7783e6a959ada2ee9317a0f6566b88e6
DustyPriest <hugo.a.obe@gmail.com>
2022-03-27 23:45:25 +0800

    basic nav; footer

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scripts for resume / portfolio webpage
+// Version 1 - 27/03/2022
+// Author: DustyPriest
+
+// FIXME: NOT YET LINKED. LINK ONCE FUNCTIONALITY IMPLEMENTED
+
+/* ------ DOCUMENT ELEMENTS ------ */
+
+const navBar = document.querySelector('nav');
