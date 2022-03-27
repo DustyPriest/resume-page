7f9758f3adef2409776a9fbbf84f2ff8733369a8
DustyPriest <hugo.a.obe@gmail.com>
2022-03-27 23:49:56 +0800

    js script set-up

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 // Version 1 - 27/03/2022
 // Author: DustyPriest
 
-// FIXME: NOT YET LINKED. LINK ONCE FUNCTIONALITY IMPLEMENTED
-
 /* ------ DOCUMENT ELEMENTS ------ */
 
 const navBar = document.querySelector('nav');
+
+// TODO: implement performant scroll listener
+// TODO: check current scroll position
+// TODO: add / remove class with box shadow from nav
