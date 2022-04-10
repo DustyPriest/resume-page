c4a8f4279599a2ae6bf712079f7b8ff7410780ad
DustyPriest <hugo.a.obe@gmail.com>
2022-04-10 13:23:17 +0800

    scroll logic, nav shadow

--- script.js
+++ script.js
@@ -2,10 +2,36 @@
 // Version 1 - 27/03/2022
 // Author: DustyPriest
 
+/* ------ GLOBALS ------ */
+
+let scrolling = false;
+
 /* ------ DOCUMENT ELEMENTS ------ */
 
 const navBar = document.querySelector('nav');
 
-// TODO: implement performant scroll listener
-// TODO: check current scroll position
-// TODO: add / remove class with box shadow from nav
+/* ------ EVENT LISTENERS ------ */
+
+window.onscroll = () => {
+  scrolling = true;
+};
+
+// throttle scroll handling
+setInterval(() => {
+  if (scrolling) {
+    scrolling = false;
+
+    assessNavShadow();
+  }
+}, 300);
+
+const assessNavShadow = () => {
+  if (window.scrollY > 0 && !navBar.classList.contains('scroll-shadow')) {
+    navBar.classList.add('scroll-shadow');
+  } else if (
+    window.scrollY === 0 &&
+    navBar.classList.contains('scroll-shadow')
+  ) {
+    navBar.classList.remove('scroll-shadow');
+  }
+};
