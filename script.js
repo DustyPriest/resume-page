e9c6395993eb2b24c826280452db43490fcc7938
DustyPriest <hugo.a.obe@gmail.com>
2022-04-14 13:44:22 +0800

    project card border animation redone

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let scrolling = false;
 /* ------ DOCUMENT ELEMENTS ------ */
 
 const navBar = document.querySelector('nav');
+const projCards = document.querySelectorAll('.project-card > a div');
 
 /* ------ EVENT LISTENERS ------ */
 
@@ -24,6 +25,7 @@ setInterval(() => {
   }
 }, 300);
 
+// add nav shadow when not at page top
 const assessNavShadow = () => {
   if (window.scrollY > 0 && !navBar.classList.contains('scroll-shadow')) {
     navBar.classList.add('scroll-shadow');
@@ -34,3 +36,14 @@ const assessNavShadow = () => {
     navBar.classList.remove('scroll-shadow');
   }
 };
+
+// project card border animation
+projCards.forEach((div) => {
+  div.addEventListener('mouseenter', () => {
+    div.classList.add('add-pseudo');
+  });
+  div.addEventListener('mouseleave', () => {
+    div.classList.remove('add-pseudo');
+    div.classList.add('remove-pseudo');
+  });
+});
